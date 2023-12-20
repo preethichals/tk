16cb244923ee592043b35353911089c5c8cb7e5a
preethichals <108888412+preethichals@users.noreply.github.com>
2023-12-20 20:47:06 +0530

    sts

--- app/component/Header.jsx
+++ app/component/Header.jsx
@@ -40,7 +40,7 @@ function Header() {
               onClick={() => setMobileMenuOpen(true)}
             >
               <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              <Bars3Icon className="h-6 w-6 bg-white" aria-hidden="true" />
             </button>
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
